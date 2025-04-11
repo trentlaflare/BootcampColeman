1b62c92b0ff7dd4911003d47df6b2556224ef44d
Trent Coleman <trentoncoleman2001@gmail.com>
2025-04-11 12:13:20 -0400

    final change

--- FinancialCalcWeek1/src/main/java/org/example/Main.java
+++ FinancialCalcWeek1/src/main/java/org/example/Main.java
@@ -1,28 +1,36 @@
 package org.example;
 
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("How much is your initial deposit?");
+        System.out.println("How much is your Principle?");
         Scanner scanner = new Scanner(System.in);
-        int initialDeposit = scanner.nextInt();
+        double principle = scanner.nextDouble();
 
         scanner.nextLine();
         System.out.println("What is your interest rate?");
-        float interest = scanner.nextFloat();
+        double interest = scanner.nextDouble();
         double months = 12;
 
         scanner.nextLine();
         System.out.println("What is your loan length(years)?");
-        int loanLength = scanner.nextInt();
-        float monthlyInterestRate = (float) ((interest/ months)/ 100);
-        int monthlyPayments = Math.addExact(loanLength, (int) months);
+        double loanLength = scanner.nextDouble();
+        double monthlyInterestRate = ((interest/ months)/ 100);
+        int numberOfMonthlyPayments = Math.multiplyExact((int) loanLength, (int) months);
+        double l=1+ monthlyInterestRate;
+        double v = principle * l;
+        double c = loanLength * 12;
 
-
-
-
-
-
-    }
+        double n = Math.pow(l,c);
+        double right = n - 1;
+        double left = n * principle;
+        double m = left / right;
+        double totalInterest = (m * numberOfMonthlyPayments) - principle;
+        System.out.printf("Your interest is %f and your monthly payment is %f", totalInterest, m);
 }
+}
+
+
+
