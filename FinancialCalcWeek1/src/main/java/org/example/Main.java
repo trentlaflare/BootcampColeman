d519ac920952e773ac8891f63bb00ff6162ec694
Trent Coleman <trentoncoleman2001@gmail.com>
2025-04-11 10:26:10 -0400

    update

--- FinancialCalcWeek1/src/main/java/org/example/Main.java
+++ FinancialCalcWeek1/src/main/java/org/example/Main.java
@@ -10,10 +10,19 @@ public class Main {
 
         scanner.nextLine();
         System.out.println("What is your interest rate?");
-        int interest = scanner.nextInt();
+        float interest = scanner.nextFloat();
+        double months = 12;
 
         scanner.nextLine();
-        System.out.println("What is your loan length");
+        System.out.println("What is your loan length(years)?");
+        int loanLength = scanner.nextInt();
+        float monthlyInterestRate = (float) ((interest/ months)/ 100);
+        int monthlyPayments = Math.addExact(loanLength, (int) months);
+
+
+
+
+
 
     }
 }
