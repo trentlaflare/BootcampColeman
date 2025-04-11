fce730b923b80abfdfffec9949b3526b1fb95817
Trent Coleman <trentoncoleman2001@gmail.com>
2025-04-11 15:22:26 -0400

    Here

--- FinancialCalcWeek1/src/main/java/Financialcalc2.java
+++ FinancialCalcWeek1/src/main/java/Financialcalc2.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Financialcalc2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your initial deposit?");
+        double deposit = scanner.nextDouble();
+
+        System.out.println("What is the annual interest rate?");
+        double interestRate = scanner.nextDouble();
+        double interest = interestRate/ 100;
+
+        System.out.println("What is the number of years?");
+        double years = scanner.nextDouble();
+
+        double fV = deposit * Math.pow(1 + interest/ 365, 365 * years);
+
+        double dailyInterest = interestRate / 365;
+
+        double totalInterest = fV - deposit;
+
+        System.out.printf("Your Future Value is $%.2f "
+                + "and your total interest earned is $%.2f", fV, totalInterest);
+
+
+
+
+
+
+    }
 }
--- FinancialCalcWeek1/src/main/java/org/example/Main.java
+++ FinancialCalcWeek1/src/main/java/org/example/Main.java
@@ -12,25 +12,25 @@ public class Main {
         scanner.nextLine();
         System.out.println("What is your interest rate?");
         double interest = scanner.nextDouble();
-        double months = 12;
+        double r = ((interest / 12) /100);
+
 
         scanner.nextLine();
         System.out.println("What is your loan length(years)?");
-        double loanLength = scanner.nextDouble();
-        double monthlyInterestRate = ((interest/ months)/ 100);
-        int numberOfMonthlyPayments = Math.multiplyExact((int) loanLength, (int) months);
-        double l=1+ monthlyInterestRate;
-        double v = principle * l;
-        double c = loanLength * 12;
-
-        double n = Math.pow(l,c);
-        double right = n - 1;
-        double left = n * principle;
+        double y = scanner.nextDouble();
+        int n = (int) (y*12);
+        double c = 1 + r;
+        double b = Math.pow(c,n);
+        double l = b * r;
+        double left = principle * l;
+        double right = b - 1;
         double m = left / right;
-        double totalInterest = (m * numberOfMonthlyPayments) - principle;
-        System.out.printf("Your interest is %f and your monthly payment is %f", totalInterest, m);
-}
-}
 
+
+        double totalInterest = (m * n) - principle;
+        System.out.printf("Your interest is $%.2f and your monthly payment is $%.2f", totalInterest, m);
+}
+}
+//
 
 
